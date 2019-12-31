ba5bca4c7756aa4cf09f64a1ddec688def56787b
kyubiop <59281567+kyubiop@users.noreply.github.com>
2019-12-31 21:57:22 +0530

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -1,27 +1,46 @@
+"""Restart or Terminate the bot from any chat
+Available Commands:
+.restart
+.shutdown"""
+# This Source Code Form is subject to the terms of the GNU
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html
+from telethon import events
 import asyncio
 import os
 import sys
+from userbot.utils import admin_cmd
 
 
-@command(pattern="^.restart", outgoing=True)
+@borg.on(admin_cmd("restart"))
 async def _(event):
     if event.fwd_from:
         return
-    asyncio.get_event_loop().create_task(restart(event))
+    # await asyncio.sleep(2)
+    # await event.edit("Restarting [██░] ...\n`.ping` me or `.help` to check if I am online after a lil bit.")
+    # await asyncio.sleep(2)
+    # await event.edit("Restarting [███]...\n`.ping` me or `.help` to check if I am online after a lil bit.")
+    # await asyncio.sleep(2)
+    await event.edit("Restarted. `.ping` me or `.helpme` to check if I am online")
+    await borg.disconnect()
+    # https://archive.is/im3rt
     os.execl(sys.executable, sys.executable, *sys.argv)
+    # You probably don't need it but whatever
+    quit()
 
 
-@command(pattern="^.shutdown", outgoing=True)
+@borg.on(admin_cmd("shutdown"))
 async def _(event):
     if event.fwd_from:
         return
-    asyncio.get_event_loop().create_task(shutdown(event))
+														 
 
 
-async def restart(event):
-    await bot.disconnect()
-    await event.edit("Restarting. `.ping` me or `.helpme` in a few minutes to check if I am online")
+						 
+						  
+																									
 
-async def shutdown(event):
-    await bot.disconnect()
+						  
+						  
     await event.edit("Turning off ...Manually turn me on later")
+    await borg.disconnect()
